f5e70f5d8a86ff3880c737e8bc71aa6d65156bbc
Dizzy <144509150+Axl-Jhone@users.noreply.github.com>
2026-06-21 20:21:02 +0800

    feat: apply ModuleLayout component to admin dashboard

--- v2-app/src/pages/admin/dashboard.tsx
+++ v2-app/src/pages/admin/dashboard.tsx
@@ -1,10 +1,12 @@
-import Logout from '@/components/Logout';
-
+import Logout from '@/components/layout/Logout';
+import ModuleLayout from "@/components/layout/ModuleLayout";
 export default function AdminDashboard() {
     return (
-        <div>
-        <h1>Hello Admin</h1>
-        <Logout />
-        </div>
+        <ModuleLayout>
+            <div>
+                <h1>Hello Admin</h1>
+                <Logout />
+            </div>
+        </ModuleLayout>
     );
 };
