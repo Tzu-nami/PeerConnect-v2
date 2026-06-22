220dd0c21cce20487f7529e1d7cdff1e63e2b9fa
TzuNami <friankarl05@gmail.com>
2026-06-22 17:13:34 +0800

    Add simple student dashboard for testing

---
+++ v2-app/src/pages/student/dashboard.tsx
@@ -0,0 +1,10 @@
+import Logout from "@/components/layout/Logout";
+
+export default function Home() {
+    return (
+        <div>
+            <h1>Hello Student</h1>
+            <Logout />
+        </div>
+    );
+};
