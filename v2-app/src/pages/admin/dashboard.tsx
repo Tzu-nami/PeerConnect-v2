caa9b03e0e06446341256ddd22eb969eaad84ae3
Dizzy <144509150+Axl-Jhone@users.noreply.github.com>
2026-06-25 16:11:19 +0800

    refactor: update ModuleLayout to adapt _app.tsx configuration

--- v2-app/src/pages/admin/dashboard.tsx
+++ v2-app/src/pages/admin/dashboard.tsx
@@ -1,12 +1,9 @@
 import Logout from '@/components/layout/Logout';
-import ModuleLayout from "@/components/layout/ModuleLayout";
 export default function AdminDashboard() {
     return (
-        <ModuleLayout>
-            <div>
-                <h1>Hello Admin</h1>
-                <Logout />
-            </div>
-        </ModuleLayout>
+        <div>
+            <h1>Hello Admin</h1>
+            <Logout />
+        </div>
     );
 };
