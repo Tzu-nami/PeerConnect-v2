e6236a8a306a1f507be83102b27d11d34c5c5dfc
Dizzy <144509150+Axl-Jhone@users.noreply.github.com>
2026-06-25 16:11:43 +0800

    feat: install and use sonner for notification toasts

--- v2-app/src/pages/_app.tsx
+++ v2-app/src/pages/_app.tsx
@@ -1,8 +1,12 @@
-import "@/styles/globals.css";
-import { AppProps } from "next/app";
-import ModuleLayout from "@/components/layout/ModuleLayout";
-import LandingLayout from "@/components/layout/LandingLayout";
+import "@/styles/globals.css"
+import { AppProps } from "next/app"
+import { Toaster } from "sonner"
 
+// Components
+import ModuleLayout from "@/components/layout/ModuleLayout"
+import LandingLayout from "@/components/layout/LandingLayout"
+
+// Routes
 const LANDING_ROUTES = ['/', '/mentors', '/staff', '/services', '/about-us', '/contact-us']
 const MODULE_ROUTES = ['/student/', '/mentor/', '/admin/']
 
@@ -10,17 +14,22 @@ export default function App({ Component, pageProps, router }: AppProps) {
     const isModuleRoute = MODULE_ROUTES.some(route => router.pathname.startsWith(route))
     const isLandingRoute = LANDING_ROUTES.includes(router.pathname)
 
-    if(isModuleRoute) return(
-        <ModuleLayout>
-            <Component {...pageProps} />
-        </ModuleLayout>
+    return (
+        <>
+            <Toaster position="top-right" richColors />
+            {isModuleRoute && (
+                <ModuleLayout>
+                    <Component {...pageProps} />
+                </ModuleLayout>
+            )}
+            {isLandingRoute && (
+                <LandingLayout userRole={pageProps.userRole ?? null}>
+                    <Component {...pageProps} />
+                </LandingLayout>
+            )}
+            {!isModuleRoute && !isLandingRoute && (
+                <Component {...pageProps} />
+            )}
+        </>
     )
-    
-    if(isLandingRoute) return(
-        <LandingLayout userRole={pageProps.userRole ?? null}>
-            <Component {...pageProps} />
-        </LandingLayout>
-    )
-
-    return <Component {...pageProps} />
 }
